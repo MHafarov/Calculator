88789386d390e9052f4a247649fd10deb18a4734
Michael Hafarov <michael.hafarov@gmail.com>
2023-05-16 22:02:59 +0300

    Set padding for ArithmeticWindow

--- src/main/java/com/app/calculator/windows/ArithmeticWindow.java
+++ src/main/java/com/app/calculator/windows/ArithmeticWindow.java
@@ -2,7 +2,9 @@ package com.app.calculator.windows;
 
 import com.app.calculator.abstractclasses.Window;
 import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,8 +16,12 @@ public class ArithmeticWindow extends Window{
     public void run() {
         Platform.runLater(() -> {
 
-            this.stage.setTitle("title");
-            StackPane root = new StackPane();
+            this.stage.setTitle("Calculator");
+            GridPane root = new GridPane();
+            root.setPadding(new Insets(20));
+            root.setHgap(25);
+            root.setVgap(15);
+
             Scene scene = new Scene(root, 600, 600);
             stage.setScene(scene);
 
